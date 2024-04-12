b34ad5f40d4ea15c3be622ddd7eaf0ec0ca5d11b
Malfioso <jeremsxm97@gmail.com>
2024-04-12 17:24:18 +0200

    Creating the dark & light mode toggle button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Box bg="black" minH="100vh">
+    <Box minH="100vh" >
       <Grid
         templateAreas={{
           base: `"nav"
                   "main"`,
-          lg:`"nav nav" "aside main"` //1024px  
+          lg: `"nav nav" "aside main"`, //1024px
         }}
       >
         <GridItem area="nav">
           <NavBar />
         </GridItem>
         <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          aside
-        </GridItem>
+          <GridItem area="aside" bg="gold">
+            aside
+          </GridItem>
         </Show>
         <GridItem area="main" bg="dodgerblue">
           main
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,21 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, useColorMode } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
 
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <HStack>
-      <Image src={logo} boxSize='50px'></Image>
+    <HStack display="flex" alignItems="center" justifyContent="space-between">
+      <Image src={logo} boxSize="50px"></Image>
       <Text>Nav bar</Text>
+      <IconButton
+        onClick={toggleColorMode}
+        aria-label={"Togle-them-mod"}
+        icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+      >
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </IconButton>
     </HStack>
   );
 };
