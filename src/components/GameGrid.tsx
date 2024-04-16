e306d79b9c619c1df3b1779ace965fb95289ac29
Malfioso <jeremsxm97@gmail.com>
2024-04-16 21:07:13 +0200

    Update on fetching games, making GameGrid only purpose to forming the data.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,50 +1,19 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-import apiClient, { CanceledError } from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((response) => {
-        setGames(response.data.results);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) {
-          console.log("Request was canceled");
-        } else {
-          setError(error.message);
-        }
-      });
-  }, []);
-
+  
+  const { games, error } = useGames();
+  
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       <Grid templateColumns="repeat(5, 1fr)" gap={2}>
-        {Array.isArray(games) && games.length > 0 ? (
-          games.map((game) => (
+        {games.map((game) => (
             <GridItem key={game.id} w="100%" h="10" bg="blue.500">
               {game.name}
             </GridItem>
-          ))
-        ) : (
-          <div>No games found.</div>
-        )}
+          ))}
       </Grid>
     </>
   );
